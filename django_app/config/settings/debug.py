4284e1044399d0807e5f55466595faa2a1038989
thenry <thenry0401@gmail.com>
2018-04-20 15:52:00 +0900

    주석 추가

--- django_app/config/settings/debug.py
+++ django_app/config/settings/debug.py
@@ -2,6 +2,6 @@ from .base import *
 
 config_secret_debug = json.loads(open(CONFIG_SECRET_DEBUG_FILE).read())
 
-DEBUG = True
+DEBUG = True # 디버그모드니까 Debug가 True
 
 ALLOWED_HOSTS = config_secret_debug['django']['allowed_hosts']
--- django_app/config/settings/deploy.py
+++ django_app/config/settings/deploy.py
@@ -2,6 +2,6 @@ from .base import *
 
 config_secret_deploy = json.loads(open(CONFIG_SECRET_DEPLOY_FILE).read())
 
-DEBUG = False
+DEBUG = False # 배포모드 이므로 Debug가 False
 
 ALLOWED_HOSTS = config_secret_deploy['django']['allowed_hosts']
