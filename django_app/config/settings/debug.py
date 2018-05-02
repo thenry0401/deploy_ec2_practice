da7120ad31cfa27a8527c8e386a342f25f57e3c9
thenry <thenry0401@gmail.com>
2018-05-02 17:04:33 +0900

    static_url, static_root 분리해서 작성, collectstatic 실행

--- django_app/config/settings/debug.py
+++ django_app/config/settings/debug.py
@@ -2,5 +2,9 @@ from .base import *
 
 config_secret_debug = json.loads(open(CONFIG_SECRET_DEBUG_FILE).read())
 
+# Static URLs
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+
 DEBUG = True # 디버그모드니까 Debug가 True
 ALLOWED_HOSTS = config_secret_debug['django']['allowed_hosts']
--- django_app/config/settings/deploy.py
+++ django_app/config/settings/deploy.py
@@ -2,6 +2,9 @@ from .base import *
 
 config_secret_deploy = json.loads(open(CONFIG_SECRET_DEPLOY_FILE).read())
 
-DEBUG = False # 배포모드 이므로 Debug가 False
+# Static URLs
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
+DEBUG = False # 배포모드 이므로 Debug가 False
 ALLOWED_HOSTS = config_secret_deploy['django']['allowed_hosts']
